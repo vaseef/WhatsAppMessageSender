f16f25be09989a9d4fecab9bab0f547696d9dba3
Vaseef <vaseef@gmail.com>
2020-05-25 17:26:56 +0300

    Changed to search for multiple groups

--- WhatsAppMessageSender/Form1.Designer.cs
+++ WhatsAppMessageSender/Form1.Designer.cs
@@ -28,19 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnLaunchWhatsapp = new System.Windows.Forms.Button();
-            this.btnSelectGroup = new System.Windows.Forms.Button();
-            this.btnPlaceMessage = new System.Windows.Forms.Button();
             this.txtMessage = new System.Windows.Forms.TextBox();
             this.btnSendMessage = new System.Windows.Forms.Button();
             this.txtChatName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtLogs = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLaunchWhatsapp
             // 
-            this.btnLaunchWhatsapp.Location = new System.Drawing.Point(95, 12);
+            this.btnLaunchWhatsapp.Location = new System.Drawing.Point(75, 22);
             this.btnLaunchWhatsapp.Name = "btnLaunchWhatsapp";
             this.btnLaunchWhatsapp.Size = new System.Drawing.Size(167, 36);
             this.btnLaunchWhatsapp.TabIndex = 0;
@@ -48,39 +50,19 @@
             this.btnLaunchWhatsapp.UseVisualStyleBackColor = true;
             this.btnLaunchWhatsapp.Click += new System.EventHandler(this.btnLaunchWhatsapp_Click);
             // 
-            // btnSelectGroup
-            // 
-            this.btnSelectGroup.Location = new System.Drawing.Point(95, 99);
-            this.btnSelectGroup.Name = "btnSelectGroup";
-            this.btnSelectGroup.Size = new System.Drawing.Size(167, 38);
-            this.btnSelectGroup.TabIndex = 1;
-            this.btnSelectGroup.Text = "Select Chat";
-            this.btnSelectGroup.UseVisualStyleBackColor = true;
-            this.btnSelectGroup.Click += new System.EventHandler(this.btnSelectGroup_Click);
-            // 
-            // btnPlaceMessage
-            // 
-            this.btnPlaceMessage.Location = new System.Drawing.Point(95, 195);
-            this.btnPlaceMessage.Name = "btnPlaceMessage";
-            this.btnPlaceMessage.Size = new System.Drawing.Size(167, 33);
-            this.btnPlaceMessage.TabIndex = 2;
-            this.btnPlaceMessage.Text = "Place Message";
-            this.btnPlaceMessage.UseVisualStyleBackColor = true;
-            this.btnPlaceMessage.Click += new System.EventHandler(this.btnPlaceMessage_Click);
-            // 
             // txtMessage
             // 
-            this.txtMessage.Location = new System.Drawing.Point(41, 158);
+            this.txtMessage.Location = new System.Drawing.Point(44, 193);
             this.txtMessage.Multiline = true;
             this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(275, 31);
+            this.txtMessage.Size = new System.Drawing.Size(236, 81);
             this.txtMessage.TabIndex = 3;
             // 
             // btnSendMessage
             // 
-            this.btnSendMessage.Location = new System.Drawing.Point(95, 234);
+            this.btnSendMessage.Location = new System.Drawing.Point(44, 291);
             this.btnSendMessage.Name = "btnSendMessage";
-            this.btnSendMessage.Size = new System.Drawing.Size(167, 34);
+            this.btnSendMessage.Size = new System.Drawing.Size(167, 47);
             this.btnSendMessage.TabIndex = 4;
             this.btnSendMessage.Text = "Send Message";
             this.btnSendMessage.UseVisualStyleBackColor = true;
@@ -88,44 +70,77 @@
             // 
             // txtChatName
             // 
-            this.txtChatName.Location = new System.Drawing.Point(95, 69);
+            this.txtChatName.Location = new System.Drawing.Point(44, 86);
+            this.txtChatName.Multiline = true;
             this.txtChatName.Name = "txtChatName";
-            this.txtChatName.Size = new System.Drawing.Size(167, 20);
+            this.txtChatName.Size = new System.Drawing.Size(236, 67);
             this.txtChatName.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(112, 51);
+            this.label1.Location = new System.Drawing.Point(41, 70);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.Size = new System.Drawing.Size(239, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Enter Person/Group Name";
+            this.label1.Text = "Enter Persons/Groups Name (Comma Seperated)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(139, 142);
+            this.label2.Location = new System.Drawing.Point(109, 167);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.Size = new System.Drawing.Size(106, 13);
             this.label2.TabIndex = 7;
-            this.label2.Text = "Enter Message";
+            this.label2.Text = "Enter Your  Message";
+            // 
+            // txtLogs
+            // 
+            this.txtLogs.Location = new System.Drawing.Point(310, 60);
+            this.txtLogs.MaxLength = 3276700;
+            this.txtLogs.Multiline = true;
+            this.txtLogs.Name = "txtLogs";
+            this.txtLogs.ReadOnly = true;
+            this.txtLogs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLogs.Size = new System.Drawing.Size(251, 265);
+            this.txtLogs.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(307, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Log";
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStop.BackgroundImage")));
+            this.btnStop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnStop.Location = new System.Drawing.Point(217, 291);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(49, 47);
+            this.btnStop.TabIndex = 10;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 293);
+            this.ClientSize = new System.Drawing.Size(598, 364);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLogs);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtChatName);
             this.Controls.Add(this.btnSendMessage);
             this.Controls.Add(this.txtMessage);
-            this.Controls.Add(this.btnPlaceMessage);
-            this.Controls.Add(this.btnSelectGroup);
             this.Controls.Add(this.btnLaunchWhatsapp);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Automate WhatsApp Messages - Vaseef";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,13 +149,14 @@
         #endregion
 
         private System.Windows.Forms.Button btnLaunchWhatsapp;
-        private System.Windows.Forms.Button btnSelectGroup;
-        private System.Windows.Forms.Button btnPlaceMessage;
         private System.Windows.Forms.TextBox txtMessage;
         private System.Windows.Forms.Button btnSendMessage;
         private System.Windows.Forms.TextBox txtChatName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLogs;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- WhatsAppMessageSender/Form1.cs
+++ WhatsAppMessageSender/Form1.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,9 @@ namespace WhatsAppMessageSender
     public partial class Form1 : Form
     {
         IWebDriver m_driver;
+        private delegate void SafeCallDelegate(string text);
+        private delegate void SafeCallDelegateToEnable(bool status);
+        private bool bStopSendingMessage = false;
         public Form1()
         {
             InitializeComponent();
@@ -26,70 +30,178 @@ namespace WhatsAppMessageSender
             m_driver.Url = "https://web.whatsapp.com/";
             m_driver.Manage().Window.Maximize();
         }
-
-        private void btnSelectGroup_Click(object sender, EventArgs e)
+        
+        private void WriteTextSafe(string text)
         {
-            string itm = txtChatName.Text;
-            string className = "_2wP_Y";
-            try
+            if (txtLogs.InvokeRequired)
             {
-                var chatList = m_driver.FindElements(By.ClassName(className));
-
-                foreach (IWebElement element in chatList)
-                {
-                    if (element != null && !string.IsNullOrEmpty(element.Text))
-                    {
-                        if (element.Text.StartsWith(itm))
-                        {
-                            element.Click();
-                        }
-                    }              
-                }
+                var d = new SafeCallDelegate(WriteTextSafe);
+                txtLogs.Invoke(d, new object[] { text });
             }
-            catch (Exception ee)
+            else
             {
-                MessageBox.Show("Class not found");
+                txtLogs.Text = text;
             }
         }
 
-        private void btnPlaceMessage_Click(object sender, EventArgs e)
+        private void EnableSendButton(bool status)
         {
-            string message = txtMessage.Text;
-            string classname = "_1Plpp";
-            try
+            if (btnSendMessage.InvokeRequired)
             {
-                var messageTextBox = m_driver.FindElement(By.ClassName(classname));
-
-                if (messageTextBox != null)
-                {
-                    messageTextBox.Click();
-
-                    messageTextBox.SendKeys(message);
-                }
+                var d = new SafeCallDelegateToEnable(EnableSendButton);
+                btnSendMessage.Invoke(d, new object[] { status });
             }
-            catch (Exception ee)
+            else
             {
-                MessageBox.Show("Class not found");
+                btnSendMessage.Enabled = status;
             }
         }
 
+        StringBuilder strLogs = new StringBuilder();
+        private void SetText(string logString)
+        {
+            strLogs.AppendLine(logString);
+            WriteTextSafe(strLogs.ToString());
+        }
+
+        Thread threadMessages = null;
         private void btnSendMessage_Click(object sender, EventArgs e)
         {
-            string classname = "_35EW6";
-            // Store locator values of email text box and sign up button		
-            try
-            {
-                var sendButton = m_driver.FindElement(By.ClassName(classname));
+            threadMessages = new Thread(new ThreadStart(SendMessages));
+            threadMessages.Start();
+        }
 
-                if (sendButton != null)
+        private void SendMessages()
+        {
+            bStopSendingMessage = false;
+
+            strLogs.Clear();
+            SetText("============Starting=============");
+            EnableSendButton(false);
+            // Get to address to list and return if it is empty.
+            List<string> toChatsList = txtChatName.Text.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            if (toChatsList.Count < 1)
+            {
+                MessageBox.Show("Please Enter Any person/group names");
+                btnSendMessage.Enabled = true;
+
+                return;
+            }
+
+            // Check whether the user entered any message or not.
+            string messageToSend = txtMessage.Text;
+            if (string.IsNullOrEmpty(messageToSend))
+            {
+                MessageBox.Show("Please Enter a message to send");
+                btnSendMessage.Enabled = true;
+                return;
+            }
+
+            // send messages.
+            int sleepTime = 1000;
+            foreach (string toChat in toChatsList)
+            {
+                try
                 {
-                    sendButton.Click();
+                    //Chat Name = toChat
+                    // Message to Send = messageToSend
+                    string toChatTrimmed = toChat.Trim();
+                    // Search the chat in search box.
+                    SetText($"Searching for chat {toChatTrimmed}: ");
+                    Thread.Sleep(sleepTime);
+                    var searchLense = m_driver.FindElement(By.ClassName("C28xL"));
+                    Thread.Sleep(sleepTime);
+                    searchLense.Click();
+                    Thread.Sleep(sleepTime);
+                    m_driver.SwitchTo().ActiveElement().SendKeys(toChatTrimmed);
+                    Thread.Sleep(sleepTime);
+
+                    // Get the chat list. List size should be grater than 0.
+                    var chatSearchResultList = m_driver.FindElements(By.ClassName("_2wP_Y"));
+                    Thread.Sleep(sleepTime);
+                    bool bMessageSent = false;
+                    foreach (IWebElement chatResultItem in chatSearchResultList)
+                    {
+                        if (chatResultItem != null && !string.IsNullOrEmpty(chatResultItem.Text))
+                        {
+                            string nameMsg = chatResultItem.Text.Split(new[] { '\r', '\n' }).FirstOrDefault();
+                            if (nameMsg.Equals(toChatTrimmed))
+                            {
+                                chatResultItem.Click();
+                                bMessageSent = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (bMessageSent)
+                    {
+                        SetText($"Found.");
+                    }
+                    else
+                    {
+                        SetText($"Not Found.");
+                    }
+                    Thread.Sleep(sleepTime);
+                    // Placing the message
+                    string messageEntryField = "_1Plpp";
+                    try
+                    {
+                        var messageTextBox = m_driver.FindElement(By.ClassName(messageEntryField));
+                        Thread.Sleep(sleepTime);
+                        if (messageTextBox != null)
+                        {
+
+                            messageTextBox.Click();
+                            Thread.Sleep(sleepTime);
+                            messageTextBox.SendKeys(messageToSend);
+                            SetText($"Message Placed Successfully.");
+                        }
+                    }
+                    catch (Exception ee)
+                    {
+                        SetText($"MessageBox Not Found.");
+                    }
+
+                    if (bStopSendingMessage)
+                    {
+                        EnableSendButton(true);
+                        SetText("============Ending==============");
+                        return;
+                    }
+                    Thread.Sleep(sleepTime);
+                    // send message by clicking the send button
+                    string sendMessageButton = "_35EW6";
+                    // Store locator values of email text box and sign up button		
+                    try
+                    {
+                        var sendButton = m_driver.FindElement(By.ClassName(sendMessageButton));
+                        Thread.Sleep(sleepTime);
+                        if (sendButton != null)
+                        {
+                            sendButton.Click();
+                            Thread.Sleep(sleepTime);
+                            SetText($"Message Send Successfully.");
+                        }
+                    }
+                    catch (Exception ee)
+                    {
+                        SetText($"Error in sending message.");
+                    }
+                }
+                catch(Exception ex1)
+                {
+                    MessageBox.Show($"Error :: {ex1.Message}");
                 }
             }
-            catch (Exception ee)
-            {
-                MessageBox.Show("Class not found");
-            }
+
+            EnableSendButton(true);
+
+            SetText("============Ending==============");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            bStopSendingMessage = true;
         }
     }
 }
